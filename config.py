6724af81e8ed840a0f8ee8d047bf13b772360800
Alina <wuangelina40@gmail.com>
2025-07-07 22:18:24 -0700

    Update config.py

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ config.py
 # [DEBUG] Enable/disable logging
 enable_logging = False  # Set to True to enable logging
 
-# [FUN] Enable/disable object forwarding for slots.py
+# [FUN] Enable/disable object forwarding from processor.py to slots.py
 enable_slots_socket = False # Set to True to enable slots socket functionality (Required to pass objects to slots.py over IPC port 54171)
